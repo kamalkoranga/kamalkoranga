6ff471bcf3062e4e15d37836117e0fb6d5166c8d
KLKA <kamalkoranga75@gmail.com>
2025-08-14 22:46:58 +0530

    auto update project count and disable context menu

--- index.js
+++ index.js
@@ -27,3 +27,20 @@ darkModeToogle.addEventListener('click', (e) => {
 // get the current year
 const year = new Date().getFullYear();
 document.getElementById("year").innerHTML = year;
+
+// Disable right-click context menu
+document.addEventListener("contextmenu", (event) => event.preventDefault());
+document.addEventListener("keydown", function (e) {
+  if (
+    e.key === "F12" ||
+    (e.ctrlKey &&
+      e.shiftKey &&
+      (e.key === "I" || e.key === "J" || e.key === "C")) ||
+    (e.ctrlKey && e.key === "U") ||
+    (e.ctrlKey && e.key === "u") ||
+    (e.ctrlKey && e.key === "s") ||
+    (e.ctrlKey && e.key === "S")
+  ) {
+    e.preventDefault();
+  }
+});
